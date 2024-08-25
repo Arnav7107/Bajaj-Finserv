106ba7a0760d4bbb450385f356e261287875038d
Arnav7107 <arnavverma345@gmail.com>
2024-08-25 12:11:15 +0530

    update filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
 import { useState } from "react";
 import axios from "axios";
+import Select from 'react-select';
 
 function App() {
   const [input, setInput] = useState("");
   const [selections, setSelections] = useState([]);
   const [response, setResponse] = useState({});
+  const [GETresponse, setGETResponse] = useState({});
+  const [GETerror, setGETError] = useState("");
   const [error, setError] = useState("");
 
   document.title = "21BCE5119";
 
-  // Validate JSON input
+  const options = [
+    { value: 'alphabets', label: 'Alphabets' },
+    { value: 'numbers', label: 'Numbers' },
+    { value: 'highest_lowercase_alphabet', label: 'Highest lowercase alphabet' }
+  ];
+
   const validateJSON = (input) => {
     try {
       JSON.parse(input);
@@ -20,12 +28,10 @@ function App() {
     }
   };
 
-  // Handle text input changes
   const handleInputChange = (e) => {
     setInput(e.target.value);
   };
 
-  // Handle submission
   const handleSubmit = async () => {
     if (!validateJSON(input)) {
       setError("Invalid JSON format. Please correct it and try again.");
@@ -37,10 +43,11 @@ function App() {
     try {
       const payload = JSON.parse(input);
       const result = await axios.post(
-        "http://localhost:5000/api/process",
+        "https://bajaj-finserv-be.onrender.com/bfhl",
         payload
       );
       setResponse(result.data);
+      alert("Data fetched successfully!");
     } catch (error) {
       setError(
         "Failed to fetch data from the server. Please check the server connection."
@@ -49,13 +56,22 @@ function App() {
     }
   };
 
-  // Handle dropdown selection changes
-  const handleSelectionChange = (e) => {
-    const values = Array.from(
-      e.target.selectedOptions,
-      (option) => option.value
-    );
-    setSelections(values);
+    const handleGET = async () => {
+      try {
+        const result = await axios.get(
+          "https://bajaj-finserv-be.onrender.com/bfhl",
+        );
+        setGETResponse(result.data);
+      } catch (error) {
+        setGETError(
+          "Failed to fetch data from the server. Please check the server connection."
+        );
+        console.error("Error fetching data:", error);
+      }
+    };
+
+  const handleSelectChange = (selectedOptions) => {
+    setSelections(selectedOptions);
   };
 
   return (
@@ -81,6 +97,7 @@ function App() {
         }}
       >
         <h1>{"21BCE5119"}</h1>
+        <h2 style={{color: "black"}}>POST</h2>
         <input
           type="text"
           value={input}
@@ -91,11 +108,20 @@ function App() {
         {error && <p style={{ color: "red" }}>{error}</p>}
 
         {Object.keys(response).length > 0 && (
-          <select multiple size="3" onChange={handleSelectionChange}>
-            <option value="alphabets">Alphabets</option>
-            <option value="numbers">Numbers</option>
-            <option value="highest">Highest lowercase alphabet</option>
-          </select>
+          <div style={{paddingTop: "20px"}}>
+            <Select
+              options={options}
+              isMulti
+              className="basic-multi-select"
+              classNamePrefix="select"
+              onChange={handleSelectChange}
+            />
+            
+            <b style={{color: "black"}}>Filtered Response:</b>
+            {selections.map((option) => (
+              response[option.value] && <p key={option.value}>{option.label}: {response[option.value].length ? response[option.value].join(', ') : "{}"}</p>
+            ))}
+          </div>
         )}
 
         {selections.includes("alphabets") && response.alphabets && (
@@ -104,9 +130,19 @@ function App() {
         {selections.includes("numbers") && response.numbers && (
           <p>Numbers: {response.numbers.join(", ")}</p>
         )}
-        {selections.includes("highest") && response.highest && (
-          <p>Highest Lowercase: {response.highest}</p>
+        {selections.includes("highest") && response.highest_lowercase_alphabet	 && (
+          <p>Highest Lowercase: {response.highest_lowercase_alphabet	}</p>
         )}
+
+        <div style={{paddingTop: "20px"}}>
+          <h2 style={{color: "black"}}>GET</h2>
+          <button onClick={handleGET}>GET</button>
+          {GETerror && <p style={{ color: "red" }}>{GETerror}</p>}
+
+          {Object.keys(GETresponse).length > 0 && (
+            <p>operation_code: {GETresponse.operation_code}</p>
+          )}
+        </div>
       </div>
     </div>
   );
